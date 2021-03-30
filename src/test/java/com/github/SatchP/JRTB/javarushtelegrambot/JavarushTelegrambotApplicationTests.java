67a1a06021e07bce3779c64511cd4474727d7d2b
”Satch” <Satch9111@gmail.com>
2021-03-30 17:10:11 +0400

    Сборка SpringBoot. Тест сборки пройден.

---
+++ src/test/java/com/github/SatchP/JRTB/javarushtelegrambot/JavarushTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SatchP.JRTB.javarushtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
