521e7dbf5f512b4986d5c4e6f605e2a404d102d4
”Satch” <Satch9111@gmail.com>
2021-03-31 14:40:23 +0400

    изменение pom.xml

--- src/main/java/com/JavarushTelegrambotApplication.java
+++ src/main/java/com/JavarushTelegrambotApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class JavarushTelegrambotApplication {
