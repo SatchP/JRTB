af288ad22f85d3f45f93a364f39dbbbe797b4685
”Satch” <Satch9111@gmail.com>
2021-06-10 15:59:05 +0400

    Исправлен прохождение теста StartComad

--- src/test/java/com/github/SatchP/JRTB/javarushtelegrambot/StartCommandTest.java
+++ src/test/java/com/github/SatchP/JRTB/javarushtelegrambot/StartCommandTest.java
@@ -1,10 +1,12 @@
 package com.github.SatchP.JRTB.javarushtelegrambot;
 
 import com.github.SatchP.JRTB.javarushtelegrambot.command.Command;
+import com.github.SatchP.JRTB.javarushtelegrambot.command.StartComand;
 import com.github.SatchP.JRTB.javarushtelegrambot.command.StopCommand;
 
 import static com.github.SatchP.JRTB.javarushtelegrambot.command.Commandname.START;
 import static com.github.SatchP.JRTB.javarushtelegrambot.command.StartComand.Start_message;
+
 
 public class StartCommandTest extends AbstractCommandTest {
     @Override
@@ -19,6 +21,6 @@ public class StartCommandTest extends AbstractCommandTest {
 
     @Override
     Command getCommand() {
-        return new StopCommand( sendBotMessageService );
+        return new StartComand( sendBotMessageService );
     }
 }
